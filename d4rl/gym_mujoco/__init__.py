7dd89a8ff982fd2ef91c8bf37053d0357e7b2e3f
aviralkumar2907 <aviralkumar2907@gmail.com>
2020-05-04 11:01:30 -0700

    Add ant datasets

--- d4rl/gym_mujoco/__init__.py
+++ d4rl/gym_mujoco/__init__.py
@@ -4,10 +4,12 @@ from d4rl.gym_mujoco import gym_envs
 HOPPER_RANDOM_SCORE = -20.272305
 HALFCHEETAH_RANDOM_SCORE = -280.178953
 WALKER_RANDOM_SCORE = 1.629008
+ANT_RANDOM_SCORE = -325.6
 
 HOPPER_EXPERT_SCORE = 3234.3
 HALFCHEETAH_EXPERT_SCORE = 12135.0
 WALKER_EXPERT_SCORE = 4592.3
+ANT_EXPERT_SCORE = 3879.7
 
 # Single Policy datasets
 register(
@@ -174,5 +176,58 @@ register(
         'ref_min_score': HOPPER_RANDOM_SCORE,
         'ref_max_score': HOPPER_EXPERT_SCORE,
         'dataset_url':'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/hopper_medium_expert.hdf5'
+    }
+)
+
+register(
+    id='ant-medium-expert-v0',
+    entry_point='d4rl.gym_mujoco.gym_envs:get_ant_env',
+    max_episode_steps=1000,
+    kwargs={
+        'dataset_url':'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/ant_medium_expert.hdf5'
+    }
+)
+
+register(
+    id='ant-mixed-v0',
+    entry_point='d4rl.gym_mujoco.gym_envs:get_ant_env',
+    max_episode_steps=1000,
+    kwargs={
+        'ref_min_score': ANT_RANDOM_SCORE,
+        'ref_max_score': ANT_EXPERT_SCORE,
+        'dataset_url':'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/ant_mixed.hdf5'
+    }
+)
+
+register(
+    id='ant-medium-v0',
+    entry_point='d4rl.gym_mujoco.gym_envs:get_ant_env',
+    max_episode_steps=1000,
+    kwargs={
+        'ref_min_score': ANT_RANDOM_SCORE,
+        'ref_max_score': ANT_EXPERT_SCORE,
+        'dataset_url':'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/ant_medium.hdf5'
+    }
+)
+
+register(
+    id='ant-random-v0',
+    entry_point='d4rl.gym_mujoco.gym_envs:get_ant_env',
+    max_episode_steps=1000,
+    kwargs={
+        'ref_min_score': ANT_RANDOM_SCORE,
+        'ref_max_score': ANT_EXPERT_SCORE,
+        'dataset_url':'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/ant_random.hdf5'
+    }
+)
+
+register(
+    id='ant-expert-v0',
+    entry_point='d4rl.gym_mujoco.gym_envs:get_ant_env',
+    max_episode_steps=1000,
+    kwargs={
+        'ref_min_score': ANT_RANDOM_SCORE,
+        'ref_max_score': ANT_EXPERT_SCORE,
+        'dataset_url':'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/ant_expert.hdf5'
     }
 )
