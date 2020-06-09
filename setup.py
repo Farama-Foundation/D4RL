9b1eeab56d2adc5afa0541b6dd95399de9635390
Justin Fu <justinjfu@gmail.com>
2020-06-08 19:44:00 -0700

    Add adept files

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(
                       'numpy', 
                       'mujoco_py', 
                       'h5py', 
+                      'termcolor', # adept_envs dependency
+                      'click',  # adept_envs dependency
+                      'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control',
                       'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
     packages=find_packages(),
 )
