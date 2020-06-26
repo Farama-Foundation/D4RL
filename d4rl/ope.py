e20493bd74f0c3868a5fb5216cfdae432d1385d1
Justin Fu <justinjfu@gmail.com>
2020-06-25 23:03:25 -0700

    typos

--- d4rl/ope.py
+++ d4rl/ope.py
@@ -11,34 +11,34 @@ POLICY_RETURNS = {
     'hopper-expert' : 3624.4696022560997,
     'walker2d-medium' : 2760.3310101980005,
     'walker2d-random' : 896.4751989935487,
-    'walker2d-expert' : .005.89370727539,
+    'walker2d-expert' : 4005.89370727539,
 }
 
 def ranking_metric(policies):
     """
     Returns true if policies given are ranked properly, from
-    highest to lowest returns.
+    highest to lowest value.
 
     Args:
         policies: A list of policy string identifiers.
             Valid identifiers must be contained in POLICY_RETURNS.
 
     Returns:
-        true if the returns of policies are monotonically decreasing.
+        True if the values of policies are monotonically decreasing.
     """
     return_values = [POLICY_RETURNS[policy_key] for policy_key in policies]
     return all(x>y for x, y in zip(return_values, return_values[1:]))
 
 
-def return_mse_metric(policy, returns):
+def value_error_metric(policy, value):
     """
-    Returns the squared error in estimated returns.
+    Returns the absolute error in estimated value.
 
     Args:
         policy (str): A policy string identifier.
-        returns (float): Estimated returns
+        value (float): Estimated value
     """
-    return (returns - POLICY_RETURNS[policy])**2
+    return abs(value - POLICY_RETURNS[policy])
 
 
 def policy_regret_metric(policy, expert_policies):
@@ -49,7 +49,7 @@ def policy_regret_metric(policy, expert_policies):
         policy (str): A policy string identifier.
         expert_policies (list[str]): A list of expert policies
     Returns:
-        The regret, which is returns of the best expert minus the returns of the policy.
+        The regret, which is value of the best expert minus the value of the policy.
     """
-    best_returns = max([POLICY_RETURNS[policy_key] for policy_key in policies])
-    return best_returns - POLICY_RETURNS[best_guess_policy]
+    best_returns = max([POLICY_RETURNS[policy_key] for policy_key in expert_policies])
+    return best_returns - POLICY_RETURNS[policy]
