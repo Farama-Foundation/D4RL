c0095c78c3fed9f3b12955ed8397298398c0879d
Justin Fu <justinjfu@gmail.com>
2021-02-05 19:44:52 -0800

    Refactor generation scripts

--- scripts/relabel_maze2d_script.sh
+++
@@ -1,7 +0,0 @@
-for MAZE in open umaze medium large
-do
-    #python scripts/relabel_maze2d_rewards.py --maze=$MAZE --filename=maze2d-$MAZE.hdf5 --relabel_type=sparse
-    #python scripts/relabel_maze2d_rewards.py --maze=$MAZE --filename=maze2d-$MAZE.hdf5 --relabel_type=dense
-    python scripts/relabel_maze2d_rewards.py --maze=$MAZE --filename=maze2d-$MAZE-noisy.hdf5 --relabel_type=sparse
-    python scripts/relabel_maze2d_rewards.py --maze=$MAZE --filename=maze2d-$MAZE-noisy.hdf5 --relabel_type=dense
-done
