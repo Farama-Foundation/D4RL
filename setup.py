e9ae51c8cc84722666e554837e18aaa4d43718df
Johannes Hackbarth <j.hackbarth@cubegaming.de>
2022-03-23 13:56:44 +0100

    Change urls in setup from unencrypted git to https Since Github deprecated the use of the unencrypted git protocol in 2021 and shut it down on 2022-03-15, the urls need https in order to continue working properly. For reference see https://github.blog/2021-09-01-improving-git-protocol-security-github/

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup(
                       'termcolor',  # adept_envs dependency
                       'click',  # adept_envs dependency
                       'dm_control' if 'macOS' in platform() else
-                      'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control',
-                      'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
+                      'dm_control @ git+https://github.com/deepmind/dm_control@master#egg=dm_control',
+                      'mjrl @ git+https://github.com/aravindr93/mjrl@master#egg=mjrl'],
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
                            'hand_manipulation_suite/assets/*',
