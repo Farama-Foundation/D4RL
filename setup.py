d7c988d329679597909f679f83dbef40b58365a5
wangtao <865471687@qq.com>
2022-04-10 15:57:30 +0800

    modify setup.py
    specify the version

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ from setuptools import find_packages
 setup(
     name='d4rl',
     version='1.1',
-    install_requires=['gym',
+    install_requires=['gym==0.19.0',
                       'numpy',
-                      'mujoco_py',
+                      'mujoco_py==2.0.2.8',
                       'pybullet',
                       'h5py',
                       'termcolor',  # adept_envs dependency
                       'click',  # adept_envs dependency
                       'dm_control' if 'macOS' in platform() else
-                      'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control',
-                      'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
+                      'dm_control @ git+https://github.com/deepmind/dm_control@644d9e0047f68b35a6f8b79e5e8493e2910563af',
+                      'mjrl @ git+https://github.com/aravindr93/mjrl@master#egg=mjrl'],
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
                            'hand_manipulation_suite/assets/*',
