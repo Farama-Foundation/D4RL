84c70f8d2604a824eb24ac9bf2d94e42018fcbe9
Ming Zhou <kornbergfresnel@outlook.com>
2022-06-20 16:36:02 +0800

    update env register

--- d4rl/grfootball/__init__.py
+++ d4rl/grfootball/__init__.py
@@ -2,18 +2,23 @@ from gym.envs.registration import register
 
 
 scenario_configs = {
-    'Full-Easy-Stochastic-v0': {
+    '11_vs_11_easy_stochastic': {
         'map_name': '11_vs_11_easy_stochastic',
         'n_right_players': 11,
         'n_left_players': 11
     },
-    'Half-v0': {
+    '5_vs_5': {
         'map_name': '5_vs_5',
         'n_right_players': 5,
         'n_left_players': 5
     },
-    'Single-v0': {
+    '1_vs_1_easy': {
         'map_name': '1_vs_1_easy',
+        'n_right_players': 1,
+        'n_left_players': 1
+    },
+    'academy_counterattack_easy': {
+        'map_name': 'academy_counterattack_easy',
         'n_right_players': 1,
         'n_left_players': 1
     }
@@ -22,26 +27,5 @@ scenario_configs = {
 
 register(
     id='GRFootball-v0',
-    entry_point='d4rl.grfootball.grf_envs:get_env',
-    # kwargs={
-    #     'scenario_config': {
-    #         **configs
-    #     }
-    # }
+    entry_point='d4rl.grfootball.grf_envs:get_env'
 )
-
-    # for dataset in ['random', 'medium', 'expert', 'medium-expert', 'medium-replay']:
-    #     env_name = 'grf-%s-%s-v0' % (scenario, dataset)
-    #     register(
-    #         id=env_name,
-    #         entry_point='d4rl.grfootball.grf_envs:get_env',
-    #         max_episode_steps=3000,
-    #         kwargs={
-    #             'scenario_config': {
-    #                 **configs
-    #             },
-    #             'ref_min_score': infos.REF_MIN_SCORE[env_name],
-    #             'ref_max_score': infos.REF_MAX_SCORE[env_name],
-    #             'dataset_url': infos.DATASET_URLS[env_name]
-    #         }
-    #     )
