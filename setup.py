e803515d0fbbc7a504ac1aae37d82d09454ad922
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2022-07-18 22:53:23 -0400

    d4rl not compatible with gym 0.25.0 yet

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 setup(
     name='d4rl',
     version='1.1',
-    install_requires=['gym',
+    install_requires=['gym<0.25.0',
                       'numpy',
                       'mujoco_py',
                       'pybullet',
