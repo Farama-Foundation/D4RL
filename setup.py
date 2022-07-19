7ffa6a80ff1a06f43a1e633bebca021a8c9125ac
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2022-07-18 22:18:01 -0400

    fix dm_control version

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from distutils.core import setup
-from platform import platform
 
 from setuptools import find_packages
 
@@ -13,8 +12,7 @@ setup(
                       'h5py',
                       'termcolor',  # adept_envs dependency
                       'click',  # adept_envs dependency
-                      'dm_control' if 'macOS' in platform() else
-                      'dm_control @ git+https://github.com/deepmind/dm_control@main#egg=dm_control',
+                      'dm_control>=1.0.3',
                       'mjrl @ git+https://github.com/aravindr93/mjrl@master#egg=mjrl'],
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
