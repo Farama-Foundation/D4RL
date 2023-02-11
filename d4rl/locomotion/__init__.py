18bd8a7f17b9625c0bbd6c63b258a0fcb4222918
One <imone@tuta.io>
2023-02-11 20:50:34 +0800

    antmaze-ultra-v2

--- d4rl/locomotion/__init__.py
+++ d4rl/locomotion/__init__.py
@@ -21,40 +21,6 @@ register(
 """
 
 register(
-    id='antmaze-ultra-diverse-v0',
-    entry_point='d4rl.locomotion.ant:make_ant_maze_env',
-    max_episode_steps=2000,
-    kwargs={
-        'deprecated': True,
-        'maze_map': maze_env.ULTRA_MAZE_TEST,
-        'reward_type':'sparse',
-        'non_zero_reset':False,
-        'dataset_url':'https://github.com/ZhengyaoJiang/d4rl/releases/download/public/Ant_maze_ultra_noisy_multistart_True_multigoal_True_sparse.hdf5',
-        'eval':True,
-        'maze_size_scaling': 4.0,
-        'ref_min_score': 0.0,
-        'ref_max_score': 1.0,
-    }
-)
-
-register(
-    id='antmaze-ultra-play-v0',
-    entry_point='d4rl.locomotion.ant:make_ant_maze_env',
-    max_episode_steps=2000,
-    kwargs={
-        'deprecated': True,
-        'maze_map': maze_env.ULTRA_MAZE_TEST,
-        'reward_type':'sparse',
-        'non_zero_reset':False,
-        'dataset_url':'https://github.com/ZhengyaoJiang/d4rl/releases/download/public/Ant_maze_ultra_noisy_multistart_True_multigoal_False_sparse.hdf5',
-        'eval':True,
-        'maze_size_scaling': 4.0,
-        'ref_min_score': 0.0,
-        'ref_max_score': 1.0,
-    }
-)
-
-register(
     id='antmaze-umaze-v0',
     entry_point='d4rl.locomotion.ant:make_ant_maze_env',
     max_episode_steps=700,
@@ -457,3 +423,38 @@ register(
     }
 )
 
+register(
+    id='antmaze-ultra-diverse-v2',
+    entry_point='d4rl.locomotion.ant:make_ant_maze_env',
+    max_episode_steps=2000,
+    kwargs={
+        'deprecated': True,
+        'maze_map': maze_env.ULTRA_MAZE_TEST,
+        'reward_type':'sparse',
+        'non_zero_reset':False,
+        'dataset_url':'https://github.com/ZhengyaoJiang/d4rl/releases/download/public/Ant_maze_ultra_noisy_multistart_True_multigoal_True_sparse.hdf5',
+        'eval':True,
+        'maze_size_scaling': 4.0,
+        'ref_min_score': 0.0,
+        'ref_max_score': 1.0,
+        'v2_resets': True,
+    }
+)
+
+register(
+    id='antmaze-ultra-play-v2',
+    entry_point='d4rl.locomotion.ant:make_ant_maze_env',
+    max_episode_steps=2000,
+    kwargs={
+        'deprecated': True,
+        'maze_map': maze_env.ULTRA_MAZE_TEST,
+        'reward_type':'sparse',
+        'non_zero_reset':False,
+        'dataset_url':'https://github.com/ZhengyaoJiang/d4rl/releases/download/public/Ant_maze_ultra_noisy_multistart_True_multigoal_False_sparse.hdf5',
+        'eval':True,
+        'maze_size_scaling': 4.0,
+        'ref_min_score': 0.0,
+        'ref_max_score': 1.0,
+        'v2_resets': True,
+    }
+)
